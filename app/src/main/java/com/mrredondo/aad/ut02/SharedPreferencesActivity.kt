89ab3c7d71e959e5747e3991159b356e865e87f7
redondoM20 <marcos.robred@educa.jcyl.es>
2021-10-07 19:46:48 +0200

    ADD

---
+++ app/src/main/java/com/mrredondo/aad/ut02/SharedPreferencesActivity.kt
@@ -0,0 +1,12 @@
+package com.mrredondo.aad.ut02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrredondo.aad.R
+
+class SharedPreferencesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preferences)
+    }
+}
